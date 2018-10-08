2837974dac5e9c0bfa335039cb9f6a4c0ea365d0
Thierry <kasskq@gmail.com>
2018-10-08 14:50:41 +0200

    add ex04 05

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
 #write your code here
+def echo(name)
+	return name
+end
+
+def shout(name)
+	return name.upcase
+end
+
+def repeat(string,n = 2)
+	result_str = ""
+	n.times do
+		result_str = result_str + " #{string}"
+	end
+	result_str[0] = ""
+	return result_str
+end
+
+def start_of_word(string, n)
+	return string[0...n]
+end
+
+def first_word(string)
+	return string.split[0]
+end
+
+def titleize(string)
+	array = string.capitalize.split(' ')
+	#puts array
+	array.each do |word|
+		if word.length > 3
+			word.capitalize!
+		end
+	end
+	result_string = array.join(' ')
+	#puts result_string
+	return result_string
+end
+
+
--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,16 @@
 #write your code here
+def time_string(input)
+	hour = min = sec = 0
+	hour = input / 3600
+	min = (input - hour * 3600) / 60
+	sec = input - hour * 3600 - min * 60
+	# sec= input%60
+	# min = (input%3600/60).to_i
+	# hour = (input/3600).to_i
+
+	str_hour = format('%02d', hour)
+	str_min = format('%02d', min)
+	str_sec = format('%02d', sec)
+
+	return "#{str_hour}:#{str_min}:#{str_sec}"
+end
