dc7fc31d56d985ba702cdab3cf2b0e9cc4c7b81f
Clem Baldy <clemoun@yahoo.fr>
2018-10-08 11:19:25 +0200

    exercices 0, 1 et 2

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add(a,b)
+	return a+b
+end
+
+def subtract(a,b)
+	return a-b
+end
+
+def sum(array)
+	somme = 0
+	array.each do |num|
+		somme += num
+	end
+	return somme
+end
+
+def multiply(a,b)
+	return a*b
+end
+
+def power(a,b)
+	return a**b
+end
+
+def factorial(a)
+	count = 1
+	if a == 0
+		return 1
+	end
+	a.times do |i|
+		count *= (i+1)
+	end
+	return count
+end
+
