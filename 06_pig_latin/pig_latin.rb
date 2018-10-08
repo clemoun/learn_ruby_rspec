bc7652611379db52bb26a89f1a54677c76f88391
Thierry <kasskq@gmail.com>
2018-10-08 15:24:03 +0200

    add ex06

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+def word_trans(string)
+	prefix = ""
+	vowel_ind = string =~ /[aeiouy]/
+	qu_ind = string =~ /qu/
+	if vowel_ind != 0
+		if qu_ind && qu_ind < vowel_ind
+			prefix = string[0...qu_ind+2]
+			string[0...qu_ind+2] = ""
+		else
+			prefix = string[0...vowel_ind]
+			string[0...vowel_ind] = ""
+		end
+	end
+	return string + prefix + "ay"
+
+end
+
+def translate(string)
+	array_words = string.split(' ')
+	array_words.map! do |word|
+		word_trans(word)
+	end
+	return array_words.join(' ')
+end
