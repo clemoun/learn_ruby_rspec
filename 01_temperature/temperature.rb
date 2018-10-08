de7235126431b539ab2f58659a84c8f289428108
Clem Baldy <clemoun@yahoo.fr>
2018-10-08 13:46:32 +0200

    ruby des exos 1 et à oubliés

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temp_f)
+	return (temp_f - 32)*5/9
+end
+
+def ctof(temp_c)
+	temp = temp_c.to_f
+	return temp*9/5 + 32
+end
+
--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,41 @@
 # write your code here
+def who_is_bigger(a,b,c)
+	unless (a && b && c)
+		return "nil detected"
+	end
+	if b>a
+		if c>b
+			return "c is bigger"
+		else
+			return "b is bigger"
+		end
+	else
+		if c>a
+			return "c is bigger"
+		end
+	end
+	return "a is bigger"
+end
+
+def reverse_upcase_noLTA(string)
+	string.reverse!
+	string.upcase!
+	while(string =~ /(L|T|A)/)
+		string[string =~ /(L|T|A)/]=""
+	end
+	return string
+end
+
+def array_42(array)
+	array.each{ |num| return true if num==42 }
+	return false
+end
+
+def magic_array(array)
+	array.flatten!
+	array.sort!
+	array.delete_if{|n| n%3==0}
+	array.uniq!
+	array.each_index {|i| array[i] *=2}
+end
+
